17acfead37c57d1cc0e9353d87e99f78acc2e160
ameliarnt <amelia@mplus.software>
2024-09-23 20:45:13 +0700

    trial: remove unfold

--- main/admin.py
+++ main/admin.py
@@ -2,23 +2,26 @@ from django.contrib import admin
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group, User
-from unfold.admin import ModelAdmin
+# from unfold.admin import ModelAdmin
+
 
 from . import models
 
-admin.site.unregister(User)
-admin.site.unregister(Group)
+# admin.site.unregister(User)
+# admin.site.unregister(Group)
 
-@admin.register(User)
-class UserAdmin(BaseUserAdmin, ModelAdmin):
-    pass
+# @admin.register(User)
+# class UserAdmin(BaseUserAdmin, ModelAdmin):
+#     pass
 
 
-@admin.register(Group)
-class GroupAdmin(BaseGroupAdmin, ModelAdmin):
-    pass
+# @admin.register(Group)
+# class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+#     pass
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Group)
 admin.site.register(models.UserProfile)
 admin.site.register(models.Discussion)
 admin.site.register(models.GeneralInfo)
